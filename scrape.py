fc7c729d7524ba4f8454296a2499b130107dabab
natasjar <60739798+natasjar@users.noreply.github.com>
2021-03-25 19:04:04 +0000

    image update

--- scrape.py
+++ scrape.py
@@ -1,12 +1,26 @@
-from urllib.request import urlopen
+from selenium import webdriver
+import urllib.request
+
+from alternat.generation import Generator
+generator = Generator()
+
+DRIVERPATH = "D:\_DOCUMENTS\Code\chromedriver_win32\chromedriver.exe"
+
+def img_scrape(url):
+    driver = webdriver.Chrome(DRIVERPATH)
+    driver.get(url) #insert website
+    
+    elements = driver.find_elements_by_tag_name('img')
+    for e in elements:
+        img = url + e.get_attribute('src')
+        print(e.get_attribute ('alt'))
+        
+        filename = img.split('/')[-1]
+        urllib.request.urlretrieve(img, filename)
+        
+def generate(img):
+    generator.generate_alt_text_from_file(img, "results")
+    
 
 
-def scraper(url):
-    page = urlopen(url)
-
-    html_bytes = page.read()
-    html = html_bytes.decode("utf-8")
-
-    with open('test.txt', 'w') as f:
-        f.write(html)
 
--- urlform.py
+++ urlform.py
@@ -1,29 +1,27 @@
 from flask import Flask, render_template, request, redirect
-from scrape import scraper as scraper
+import scrape as s
 app = Flask(__name__)
 
 lst = []
 
 @app.route('/')
 def hello_world():
-    author = "Me"
-    name = "You"
-    return render_template('index.html', author=author, name=name)
+    return render_template('index.html')
 
 
 @app.route('/signup', methods = ['POST'])
 def signup():
     url = request.form['URL']
     #throws unknown url type error
-    scraper(url)
+    s.img_scrape(url)
     return redirect('/')
 
-@app.route('/emails.html')
-def emails():
-    return render_template('urls.html', email_addresses=email_addresses)
+# @app.route('/emails.html')
+# def emails():
+#     return render_template('urls.html', email_addresses=email_addresses)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8080, debug = False)
 
  
 
